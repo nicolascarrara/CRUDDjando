b899aca5adaf2516ff59bf9787edbe3b0faee1d0
Nicolas CARRARA <n.carrara28@icloud.com>
2017-10-10 11:31:14 +0200

    migration BDD

--- musiques/models.py
+++ musiques/models.py
@@ -10,8 +10,7 @@ class Artiste(models.Model):
 
 class Morceau(models.Model):
     titre = models.CharField(max_length=64)
-    artiste=models.CharField(max_length=64)
-    artiste_fk = models.ForeignKey(Artiste,on_delete=models.CASCADE,null=True)
+    artiste = models.ForeignKey(Artiste,on_delete=models.CASCADE,null=True)
     def __str__(self):
         return'{self.titre} ({self.artiste})'.format(self=self)
 
