db2b717f61514759a2daf7e739042af6ac9f9ce8
Amaël ZICKLER <amaelzickler@gmail.com>
2017-10-12 10:58:27 +0200

    Update models.py

--- musiques/models.py
+++ musiques/models.py
@@ -7,21 +7,10 @@ from django.db import models
 
 
 class Artiste(models.Model):
-<<<<<<< HEAD
-    nom=models.CharField(max_length=64)
-    
-    def __str__(self):
-        return'{self.nom}'.format(self=self)
-=======
     nom = models.CharField(max_length=64)
 
     def __str__(self):
         return'{self.nom}'.format(self=self).encode('utf-8')
-
-    def get_absolute_url(self):
-        from django.urls import reverse
-        return reverse('musiques:artiste-detail', args=[str(self.id)])
->>>>>>> d7c1951894b44fad728810c936fcd329ba379c2d
 
     def get_absolute_url(self):
         from django.urls import reverse
@@ -32,16 +21,8 @@ class Morceau(models.Model):
     artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-<<<<<<< HEAD
-        return'{self.titre}'.format(self=self)
-=======
         return'{self.titre}'.format(self=self).encode('utf-8')
->>>>>>> d7c1951894b44fad728810c936fcd329ba379c2d
 
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('musiques:morceau-detail', args=[str(self.id)])
-<<<<<<< HEAD
-    
-=======
->>>>>>> d7c1951894b44fad728810c936fcd329ba379c2d
