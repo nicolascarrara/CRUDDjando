577100553fde79f7084bf52eef1253a4df748ef2
Amaël ZICKLER <amaelzickler@gmail.com>
2017-10-13 10:00:20 +0200

    Update tests.py

--- musiques/tests.py
+++ musiques/tests.py
@@ -1,37 +1,6 @@
-from django.urls import reverse
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.test import TestCase
-from django.urls.exceptions import NoReverseMatch
 
-from musiques.models import Morceau
-
-
-class MorceauTestCase(TestCase):
-    def setUp(self):
-        Morceau.objects.create(titre='musique1')
-        Morceau.objects.create(titre='musique2')
-        Morceau.objects.create(titre='musique3')
-
-    def test_morceau_url_name(self):
-        try:
-            url = reverse('musiques:morceau-detail', args=[1])
-        except NoReverseMatch:
-            assert False
-
-    def test_morceau_url(self):
-        morceau = Morceau.objects.get(pk=1)
-        url = reverse('musiques:morceau-detail', args=[morceau.pk])
-        response = self.client.get(url)
-        assert response.status_code == 200
-    
-    def test_ajout_bd(self): 
-        assert Morceau.objects.filter(pk=1).exists()
-
-
-    def test_modif_bd(self):
-        Morceau.objects.filter(pk=1).update(titre='test')
-        morceau = Morceau.objects.get(pk=1)
-        assert morceau.titre=='test'
-
-
-    def test_delete_bd(self):
-        assert Morceau.objects.filter(pk=1).delete()
+# Create your tests here.
